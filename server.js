ed28386ee621f8a8c177074d1faa1d4668f3bbcc
Shubham Darekar <129769325+Shubham0D4@users.noreply.github.com>
2025-06-18 10:45:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors({
   origin: [
     'https://plumeriaretreat.vercel.app',
+    'https://plumeriaretreat.com/',
     'http://localhost:5173',
     'http://localhost:5174'
   ],
@@ -580,5 +581,9 @@ app.post('/api/payments/payu', async (req, res) => {
     payuData
   });
 });
+
+app.listen(PORT, ()=>{
+  console.log("server started at 5000")
+}
 
 export default app;
